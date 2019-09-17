2372e7eb1ccfaaa348e441cb3b2d6eb320993782
신동철 <ruso@sindongcheol-ui-MacBookPro.local>
2019-09-17 18:48:43 +0900

    내부 주요로직은 action에서 진행되도록 설정

---
+++ src/config/action/enhance.js
@@ -0,0 +1,16 @@
+const dbApi = require('../database/dbapi');
+
+module.exports = {
+  getEnhanceSearchItem : async (params) => {
+
+    let sql;
+
+    if (params.cate === 'all') {
+        sql = params.name === '' ? `SELECT * FROM item` : `SELECT * FROM item WHERE name LIKE '%${params.name}%'`;
+    } else {
+        sql = (params.name === '' ? `SELECT * FROM item WHERE ` : `SELECT * FROM item WHERE name LIKE '%${params.name}%' AND `) + `cate = '${params.cate}'`;
+    }
+
+    return await dbApi.selectQuery(sql);
+  }
+}
--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 6050;
 const dbApi = require('./config/database/dbapi');
+const enhanceApi = require('./config/action/enhance');
 
+
+app.use(cors());
 app.use(express.json());
 
 app.get('/data', async (req,res)=>{
@@ -10,16 +14,16 @@ app.get('/data', async (req,res)=>{
         const result = await dbApi.selectQuery('SELECT * FROM `item`');
         res.send(result);
     } catch (e) {
-        console.log(e)
+        console.log(e);
     } finally {
         conn.end();
     }
 })
 
-app.post('/api/enhance/dialog/input/search', (req, res) => {
-    const params = req.body;
+app.post('/api/enhance/dialog/input/search', async (req, res) => {
+    const result = await enhanceApi.getEnhanceSearchItem(req.body);
 
-    res.send(req.body);
+    res.send(result);
 })
 
 app.listen(PORT,()=>{
