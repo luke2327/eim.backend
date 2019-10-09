c0486e266c63a7c11bd6c4a57e98238bdc2399c7
luke2327 <qwepoi8526@gmail.com>
2019-10-09 21:53:39 +0900

    [방어구 인서트]

--- src/middlewares/mapleApi.js
+++ src/middlewares/mapleApi.js
@@ -5,6 +5,10 @@ const router = express.Router();
 const common = require('utils/common');
 const mysql = require('mysql');
 const dbApi = require('config/database/dbapi');
+const axios = require('axios');
+const axiosRetry = require('axios-retry');
+
+axiosRetry(axios, { retries: 3 });
 
 router.post('/item', async (req, res) => {
   req.body.path = req.url;
@@ -82,15 +86,16 @@ router.post('/input/item', async (req, res) => {
   req.body.path = req.url.replace('/input/', '');
 
   const result = await common.sendMaple(req.body);
+  console.log(result.data);
   let query_data = [];
-  const real_query = [];
+  const dataset = [];
 
   req.body.locale = 'ko';
 
   _.forEach(result.data, (data) => {
     _.map(data, (value, key) => {
       if (Array.isArray(value)) {
-        query_data.push(value.join(' '));
+        query_data.push(JSON.stringify(value.join(' ')));
       }
       else if (typeof value === 'object') {
         _.map(value, (sub_value, sub_key) => {
@@ -103,13 +108,16 @@ router.post('/input/item', async (req, res) => {
         query_data.push(typeof value === 'string' ? `"${value}"` : value);
       }
     });
-    real_query.push(query_data);
+    dataset.push(query_data);
     query_data = [];
   });
 
-  _.forEach(real_query, async (query) => {
+  _.forEach(dataset, async (query) => {
     if (req.body.subCategoryFilter === 'Miracle Cube') {
       await dbApi.selectQuery(`INSERT IGNORE INTO item_cube (is_cash, name_ko, \`desc\`, item_no, overall_category, category, sub_category, low_item_id, high_item_id) 
+      VALUES (${query.join()})`);
+    } else if (req.body.categoryFilter === 'Armor') {
+      await dbApi.selectQuery(`INSERT IGNORE INTO item_equip (req_jobs, req_level, is_cash, name_ko, \`desc\`, item_no, req_gender, overall_category, category, sub_category, low_item_id, high_item_id) 
       VALUES (${query.join()})`);
     }
   });
@@ -119,7 +127,7 @@ router.post('/input/item', async (req, res) => {
 
 router.post('/input/item-meta', async (req, res) => {
   const mapleReq = {};
-  const insertData = {};
+  let insertData = {};
 
   if (req.body.overallCategory === 'Equip' && (_.includes(req.body.category, 'One-Handed Weapon')) || _.includes(req.body.category, 'One-Handed Weapon')) {
     const sql = `SELECT item_no FROM item_weapon WHERE
@@ -178,6 +186,73 @@ router.post('/input/item-meta', async (req, res) => {
         insertData.item_no = v.item_no;
         console.log(insertData);
         const insert_sql = mysql.format('INSERT IGNORE INTO weapon_meta SET ?', insertData);
+
+        console.log(insert_sql);
+        await dbApi.selectQuery(insert_sql);
+      }
+    });
+  } else if (req.body.overallCategory === 'Equip' && req.body.category === 'Armor') {
+    const sql = `SELECT item_no FROM item_equip WHERE
+      req_level >= ${req.body.minLevelFilter}
+      AND req_level <= ${req.body.maxLevelFilter}
+      AND overall_category = '${req.body.overallCategory}'
+      AND category = '${req.body.category}'`;
+
+    const result = JSON.parse(JSON.stringify(await dbApi.selectQuery(sql)));
+
+    _.forEach(result, async (v) => {
+      mapleReq.path = `item/${v.item_no}`;
+      mapleReq.locale = req.body.locale;
+      const mapleRes = await common.sendMaple(mapleReq);
+      axiosRetry(axios, { retryDelay: axiosRetry.exponentialDelay});
+      insertData = {};
+
+      console.log(mapleRes.data.metaInfo);
+
+      console.log('---------------------------');
+
+      if (mapleRes.data.metaInfo || mapleRes.metaInfo) {
+        _.map(mapleRes.data.metaInfo, (value, key) => {
+          insertData[key] = value;
+        });
+
+        // 필요한 키만 추출
+        delete insertData.mob;
+        delete insertData.iconRaw;
+        delete insertData.icon;
+        delete insertData.iconOrigin;
+        delete insertData.iconRawOrigin;
+        delete insertData.price;
+        delete insertData.exItem;
+        delete insertData.bossReward;
+        delete insertData.consumeSpec;
+        delete insertData.setCompleteCount;
+        delete insertData.vslots;
+        delete insertData.islots;
+        delete insertData.equipTradeBlock;
+        delete insertData.isIot;
+        delete insertData.islot;
+        delete insertData.vsIot;
+        delete insertData.vslot;
+        delete insertData.tradeBlock;
+
+        const noTransform = ['reqSTR', 'reqDEX', 'reqINT', 'reqLUK', 'incSTR', 'incDEX', 'incINT', 'incLUK', 'incPAD', 'incMAD', 'charmEXP', 'incPDD', 'incMHP', 'incMMP', 'incMDD', 'incACC', 'incEVA'];
+
+        _.map(insertData, (value, key) => {
+          if (noTransform.includes(key)) {
+            const new_key = key.replace(/([A-Z]+)/, '_$1').toLowerCase();
+
+            delete Object.assign(insertData, { [new_key]: insertData[key] })[key];
+          } else if (key.match(/(?=[A-Z])/)) {
+            // 키를 snake_case 로 교체
+            const new_key = key.split(/(?=[A-Z])/).join('_').toLowerCase();
+
+            delete Object.assign(insertData, { [new_key]: insertData[key] })[key];
+          }
+        });
+
+        insertData.item_no = v.item_no;
+        const insert_sql = mysql.format('INSERT IGNORE INTO equip_meta SET ?', insertData);
 
         console.log(insert_sql);
         await dbApi.selectQuery(insert_sql);
