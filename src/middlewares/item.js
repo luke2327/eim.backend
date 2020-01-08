a3ba8ba4a9980a9841d9010ce0d340c288b12860
luke2327 <qwepoi8526@gmail.com>
2020-01-08 09:43:09 +0900

    API 주소 변경 및 그 외

--- src/middlewares/item.js
+++ src/middlewares/item.js
@@ -5,15 +5,22 @@ const router = express.Router();
 const simulate = require('config/action/simulate');
 const ck = require('camelcase-keys');
 
-router.post('/major-weapon', async (req, res) => {
+router.post('/getMajorWeapon', async (req, res) => {
   const result = await simulate.getItemMajorWeapon(req.body)
     .then(re => ck(re, { deep: true }));
 
   res.send(result);
 });
 
-router.post('/available-cube', async (req, res) => {
+router.post('/getItemAvailableCube', async (req, res) => {
   const result = await simulate.getItemAvailableCube(req.body)
+    .then(re => ck(re, { deep: true }));
+
+  res.send(result);
+});
+
+router.post('/getEquipmentItem', async (req, res) => {
+  const result = await simulate.getEquipmentItem(req.body)
     .then(re => ck(re, { deep: true }));
 
   res.send(result);
@@ -21,13 +28,6 @@ router.post('/available-cube', async (req, res) => {
 
 router.post('/set-potential', async (req, res) => {
   const result = await simulate.getSimulateSetPotential(req.body)
-    .then(re => ck(re, { deep: true }));
-
-  res.send(result);
-});
-
-router.post('/equipment-item', async (req, res) => {
-  const result = await simulate.getEquipmentItem(req.body)
     .then(re => ck(re, { deep: true }));
 
   res.send(result);
