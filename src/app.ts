120b85ea17dc44ae100263ad9d9a7fd192fd50c5
luke2327 <qwepoi8526@gmail.com>
2020-01-09 09:43:23 +0900

    app 주석 및 일부 수정

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,33 @@
 import express, { json, Request, Response, NextFunction } from 'express';
+import { CommonErr } from './models/core/error.interface';
 import cors from 'cors';
 import item from './routes/item.routes';
 import notice from './routes/notice.routes';
+
 const app = express();
-
-interface Err extends Error {
-  status: number
-  data?: any
-}
-
-app.use(cors());
-app.use(json());
-app.use('/api/item', item);
-app.use('/api/notice', notice);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('This run!');
 });
 
-// catch 404 and forward to error handler
+app.use(cors());
+app.use(json());
+
+/** add middlewares */
+app.use('/api/item', item);
+app.use('/api/notice', notice);
+
+/** catch 404 and forward to error handler */
 app.use((req: Request, res: Response, next: NextFunction) => {
-  let err = new Error('Not Found') as Err;
+  let err = new Error('API Address Not Found') as CommonErr;
 
   err.status = 404;
   next(err);
 });
 
-// error handle
-app.use((err: Err, req: Request, res: Response, next: NextFunction) => {
-  // render the error page
+/** error handle */
+app.use((err: CommonErr, req: Request, res: Response, next: NextFunction) => {
+  /** render the error page */
   res.status(err.status || 500);
   res.json({
     message: err.message,
